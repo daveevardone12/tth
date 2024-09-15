d73d4531c713e03992346f7accb02755c063cfae
daveevardone12 <davemarlon12@icloud.com>
2024-09-15 20:54:23 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const ejs = require("ejs");
+const path = require("path");
+const passport = require("passport");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use("/static", express.static("static"));
+app.use("/public", express.static("public"));
+
+app.get("/", async (req, res) => {
+  res.render("login");
+});
+
+app.get("/signup", async (req, res) => {
+  res.render("signup");
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server started at ${PORT}`);
+});
