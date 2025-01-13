94d7d022c2754b7104a22192eed31b784115c7c6
Pansikeeee <tabuaclloyd@gmail.com>
2025-01-13 22:42:16 +0800

    Your descriptive commit message

---
+++ Public/js/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Apply animations to content
+    const contentElements = document.querySelectorAll('.content h1, .content p, .signup');
+    contentElements.forEach((el) => {
+      setTimeout(() => {
+        el.style.opacity = '1';
+        el.style.transform = 'translateY(0)';
+      }, 500); // Delay the animation for a smooth entrance
+    });
+  });
+  
