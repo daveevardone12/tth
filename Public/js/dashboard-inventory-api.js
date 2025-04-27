bb0f32df8c69b3eada69032e485929d6f6e158f6
daveevardone12 <davemarlon12@icloud.com>
2025-04-28 00:13:23 +0800

    weww

--- Public/js/dashboard-inventory-api.js
+++ Public/js/dashboard-inventory-api.js
@@ -385,10 +385,12 @@ function printDocument(button) {
       el.classList.add("hiddenPhotos");
     });
     if (button.getAttribute("data-office")) {
+      console.log("triggerd office");
       document.body.classList.add("show-office");
+      console.log("triggerd office1");
       setTimeout(() => {
         window.print();
-      }, 500);
+      }, 2000);
     }
     if (button.getAttribute("data-table")) {
       console.log("triggerd");
